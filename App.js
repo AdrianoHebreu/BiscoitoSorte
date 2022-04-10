c1de5d88f869d3aee4dbd81193189a163a5790e4
adrianohebreu <adriano12144@icloud.com>
2022-04-10 19:32:19 +0100

    ProjetoFinalizado

--- App.js
+++ App.js
@@ -3,23 +3,46 @@ import {Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [img, setImg]  = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
+
+  let textos = [
+    'Siga os bons e aprenda com eles',
+    'O bom-senso vale mais do que muito conhecimento',
+    'O riso é a menor distancia entre duas pessoas',
+    'Deixe de lado  as preocupações e seja feliz',
+    'Realize o obvio, pense no improvavel e conquiste o impossivel',
+    'Acredite em milagres, mas não dependa deles',
+    'A maior barreira para o sucesso é o medo do fracasso'
+  ];
+
+  function quebraBiscoito(){
+    setImg(require('./src/biscoitoAberto.png'));
+    let numeroAleatorio = Math.floor(Math.random() * textos.length);
+    setTextoFrase('"' + textos[numeroAleatorio] +'"');
+  }
+
+  function reiniciarBiscoito(){
+    setImg(require('./src/biscoito.png'));
+    setTextoFrase('');
+  }
+
   return (
     <View style={style.container}>
       <Image
         source={img}
         style={style.img}
       />
-      <Text style={style.textofrase} >'Essa é a minha primeira frase no aplicativo'</Text>
+      <Text style={style.textofrase} >{textoFrase}</Text>
 
-      <TouchableOpacity style={style.btn}>
+      <TouchableOpacity style={style.btn} onPress={quebraBiscoito}>
         <View style={style.btnArea}>
           <Text style={style.btnText}>Quebar biscoito</Text>
         </View>
       </TouchableOpacity>
 
-      <TouchableOpacity style={[style.btn, {marginTop: 15, borderColor: '#121212'} ]}>
+      <TouchableOpacity style={[style.btn, {marginTop: 15, borderColor: '#121212'} ]} onPress={reiniciarBiscoito}>
         <View style={style.btnArea}>
-          <Text style={[style.btnText, {color: '#121212'}]}>Quebar biscoito</Text>
+          <Text style={[style.btnText, {color: '#121212'}]}>Reiniciar biscoito</Text>
         </View>
       </TouchableOpacity>
   
@@ -34,8 +57,8 @@ const style = StyleSheet.create({
     justifyContent: 'center'
   },
   img:{
-    width: 230,
-    height: 230
+    width: 220,
+    height: 220
   },
   textofrase: {
     fontSize: 20,
